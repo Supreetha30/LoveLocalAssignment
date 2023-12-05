8e4d844cb2cf50b6fcd7b09c38a895cf56427192
Supreetha30 <130357600+Supreetha30@users.noreply.github.com>
2023-12-05 21:00:22 +0530

    Update easy1.py

--- easy1.py
+++ easy1.py
@@ -1,24 +1,12 @@
 def length_of_last_word(s):
-    # Split the string into words using whitespaces as separators
+ 
     words = s.split()
-
-    # Check if there are any words in the list
     if words:
-        # Return the length of the last word
         return len(words[-1])
     else:
-        # If there are no words, return 0
         return 0
 
-# Example usage:
-s1 = "Hello World"
-s2 = "   fly me   to   the moon  "
-s3 = "luffy is still joyboy"
+user_input = input("Enter a string: ")
 
-output1 = length_of_last_word(s1)
-output2 = length_of_last_word(s2)
-output3 = length_of_last_word(s3)
-
-print(output1)  
-print(output2) 
-print(output3) 
+result = length_of_last_word(user_input)
+print("Length of the last word:", result) 
